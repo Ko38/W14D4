3c5bdebdd68c4fc5e80871f6320c0b65b21d3572
App Academy Student <student@appacademy.io>
2019-09-26 18:06:38 -0700

    done

--- insertion_sort_project/lib/insertion_sort.js
+++ insertion_sort_project/lib/insertion_sort.js
@@ -1,5 +1,17 @@
 function insertionSort(arr) {
-
+    for(let i = 1; i < arr.length; i++){
+        let current = arr[i];
+        for(let j = i - 1; j >= 0; j--) {
+            if (current < arr[j]){
+                arr[j+1] = arr[j];
+                arr[j] = current;
+            } else {
+                arr[j+1] = current;
+                break;
+            }
+        }
+    }
+    return arr;
 }
 
 module.exports = {
--- selection_sort_project/lib/selection_sort.js
+++ selection_sort_project/lib/selection_sort.js
@@ -1,9 +1,21 @@
 function swap(arr, index1, index2) {
-
+    let temp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp;
+    return arr;
 }
 
 function selectionSort(arr) {
-
+    for(let i = 0; i < arr.length - 1; i++){
+        let minIndex = i;
+        for(let j = i + 1; j < arr.length; j++){
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        swap(arr, minIndex, i);
+    }
+    return arr;
 }
 
 module.exports = {
